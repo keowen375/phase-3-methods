bfff596f077a370abbfbe022db2522f4fb8ac993
keowen375@gmail.com <keowen375@gmail.com>
2023-05-22 09:05:32 +0300

    Complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+
+def halve(numb)
+    if numb.class != Float && numb.class != Integer
+        return nil
+    end
+
+    return numb / 2
+end
